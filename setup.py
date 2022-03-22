bfa1a71a5ae7e3840c0283b26dc2220ba197001a
andresrosso <andresrosso@gmail.com>
2022-03-22 12:12:44 -0500

    coockie cutter setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='BioASQ10 participation code -Universidad Nacional (Bogotá-Colombia)-',
+    author='UN (BOGOTA-COLOMBIA)',
+    license='MIT',
+)
