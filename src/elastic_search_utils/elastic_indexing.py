47ae08f53b3de33fc9be754e4eab9d2e7430f5f5
jodrodriguezpo <jodrodriguezpo@unal.edu.co>
2022-03-23 21:26:22 -0500

    Added script to index failed documents

--- src/elastic_search_utils/elastic_indexing.py
+++ src/elastic_search_utils/elastic_indexing.py
@@ -1,5 +1,6 @@
 import json
 import time
+import argparse
 from collections import deque
 from tqdm import tqdm
 from elasticsearch import Elasticsearch
@@ -19,39 +20,61 @@ path = '/datasets/pubmed2022/'
 dirs = os.listdir( path )
 read_dirs = [f"{path}{dirx}" for dirx in dirs]
 
-try:
-    es.indices.delete(index=index)
-    print('Deleted index')
-except:
-    print('Not found index')
-#es.indices.delete(index='pubmed_2022_index')
-print('CREATING')
-es.indices.create(index=index)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Parser for Bavaria training MLOps'
+    )
+    parser.add_argument(
+        '--prefix',
+        type=str,
+        required=True
+    )
+    args = parser.parse_args()
+    PREFIX = args.prefix
+    
+    print(f"USED PREFIX: {PREFIX}")
+    
+    filter_read_dirs = [
+        read_dir for read_dir in read_dirs
+        if PREFIX in read_dir
+    ]
+    print(f"KEPT {len(filter_read_dirs)} FILES FROM THE ORIGINAL {len(read_dirs)}")
+    print(f"EXAMPLE FILE: {filter_read_dirs[0]}")
 
-failures = {}
-
-print(f"INDEXING {len(read_dirs)} FILES")
-for n_file, file in enumerate(read_dirs):
-    print('\n' * 3)
-    print('*' * 20)
-    print(f"WRITING {file}, {n_file + 1}/{len(read_dirs)}")
+    # NOT DOING THIS RIGHT NOW
+    """
     try:
-        f = gzip.open(file, 'rb')
-        file_content = f.read()
-        dict_out = pp.parse_medline_xml(file_content)
-        dict_form = [
-            {
-                "_id": dictx['pmid'],
-                "_source": dictx
-            }
-            for dictx in dict_out
-        ]
-        resp = helpers.parallel_bulk(es, dict_form, index=index, chunk_size=2000)
-        deque(resp, maxlen=0)
-    except Exception as e:
-        print(f"FAILED FILE: {file} ({len(failures.keys())} files failed)")
-        logging.exception(e)
-        failures[file] = str(e)
+        #es.indices.delete(index=index)
+        print('Deleted index, fix')
+    except:
+        print('Not found index')
+    print('CREATING')
+    es.indices.create(index=index)
+    """
 
-with open('failures.json', 'w') as fails_file:
-    json.dump(failures, fails_file)
+    failures = {}
+
+    print(f"INDEXING {len(filter_read_dirs)} FILES")
+    for n_file, file in tqdm(enumerate(filter_read_dirs)):
+        print('\n' * 3)
+        print('*' * 20)
+        print(f"WRITING {file}, {n_file + 1}/{len(filter_read_dirs)}")
+        try:
+            f = gzip.open(file, 'rb')
+            file_content = f.read()
+            dict_out = pp.parse_medline_xml(file_content)
+            dict_form = [
+                {
+                    "_id": dictx['pmid'],
+                    "_source": dictx
+                }
+                for dictx in dict_out
+            ]
+            resp = helpers.bulk(es, dict_form, index=index)
+        except Exception as e:
+            print(f"FAILED FILE: {file} ({len(failures.keys())} files failed)")
+            logging.exception(e)
+            failures[file] = str(e)
+
+    with open(f'{PREFIX}_failures.json', 'w') as fails_file:
+        json.dump(failures, fails_file)
