b6184f73e872c5707895f2a84e9ea5cff1f23679
andresrosso <andresrosso@gmail.com>
2023-05-10 20:54:43 +0000

    indexing parellel

--- src/elastic_search_utils/elastic_indexing_parallel.py
+++ src/elastic_search_utils/elastic_indexing_parallel.py
@@ -12,6 +12,9 @@ import os, sys
 import pubmed_parser as pp
 from pprint import pprint
 import logging
+from joblib import Parallel, delayed
+from multiprocessing import Pool, Process, Manager
+
 
 index = 'pubmed2023-old'
 es = Elasticsearch('http://localhost:9200')
@@ -19,6 +22,30 @@ path = '/opt/bioasq/resources/pubmed_baseline_2023/'
 
 dirs = os.listdir( path )
 read_dirs = [f"{path}{dirx}" for dirx in dirs]
+failures = {}
+n_file = 0
+
+def process_file(file):
+    print('\n' * 3)
+    print('*' * 20)
+    print(f"WRITING {file}, {n_file + 1}/{len(filter_read_dirs)}")
+    try:
+        f = gzip.open(file, 'rb')
+        file_content = f.read()
+        dict_out = pp.parse_medline_xml(file_content)
+        dict_form = [
+            {
+                "_id": dictx['pmid'],
+                "_source": dictx
+            }
+            for dictx in dict_out
+        ]
+        resp = helpers.bulk(es, dict_form, index=index)
+    except Exception as e:
+        print(f"FAILED FILE: {file} ({len(failures.keys())} files failed)")
+        logging.exception(e)
+        failures[file] = str(e)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -51,29 +78,15 @@ if __name__ == "__main__":
     print('CREATING')
     es.indices.create(index=index)
 
-    failures = {}
-
     print(f"INDEXING {len(filter_read_dirs)} FILES")
-    for n_file, file in tqdm(enumerate(filter_read_dirs)):
-        print('\n' * 3)
-        print('*' * 20)
-        print(f"WRITING {file}, {n_file + 1}/{len(filter_read_dirs)}")
-        try:
-            f = gzip.open(file, 'rb')
-            file_content = f.read()
-            dict_out = pp.parse_medline_xml(file_content)
-            dict_form = [
-                {
-                    "_id": dictx['pmid'],
-                    "_source": dictx
-                }
-                for dictx in dict_out
-            ]
-            resp = helpers.parallel_bulk(es, dict_form, index=index)
-        except Exception as e:
-            print(f"FAILED FILE: {file} ({len(failures.keys())} files failed)")
-            logging.exception(e)
-            failures[file] = str(e)
+    
+    #Parallel(n_jobs=10)(delayed(process_file)(file) for file in filter_read_dirs )
 
+    pool = Pool(8)
+    df_collection = pool.map(process_file, filter_read_dirs)
+    pool.close()
+    pool.join()
+    
     with open(f'{PREFIX}_failures.json', 'w') as fails_file:
         json.dump(failures, fails_file)
+
